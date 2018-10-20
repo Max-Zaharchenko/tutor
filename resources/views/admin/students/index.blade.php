f1fb5580a8cb2a134ee3a76f723d6fa1ed1c7f8f
paulandroshchuk <paul.androshchuk@gmail.com>
2018-10-20 14:18:31 +0300

    students

--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -33,27 +33,31 @@
             </form>
         </div>
     </div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Имя</th>
-                <th>Подписка заканчивается</th>
-                <th>E-Mail</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($students as $student)
-                <tr>
-                    <td>{{ $student->getAttribute('name') }}</td>
-                    <td>{{ $student->getAttribute('trial_ends_at') }}</td>
-                    <td>{{ $student->getAttribute('email') }}</td>
-                    <td></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <div class="row justify-content-center">
-        {!! $students !!}
-    </div>
+    <div class="card">
+       <div class="card-body">
+           <table class="table">
+               <thead>
+               <tr>
+                   <th>Имя</th>
+                   <th>Подписка заканчивается</th>
+                   <th>E-Mail</th>
+                   <th></th>
+               </tr>
+               </thead>
+               <tbody>
+               @foreach($students as $student)
+                   <tr>
+                       <td>{{ $student->getAttribute('name') }}</td>
+                       <td>{{ $student->getAttribute('trial_ends_at') }}</td>
+                       <td>{{ $student->getAttribute('email') }}</td>
+                       <td></td>
+                   </tr>
+               @endforeach
+               </tbody>
+           </table>
+           <div class="row justify-content-center">
+               {!! $students !!}
+           </div>
+       </div>
+   </div>
 @endsection
