6d86db76de1b3096ab0de4e6b9fe83ff23d04a6e
Paul A <paul.androshchuk@gmail.com>
2018-12-17 22:00:49 +0200

    Add rule.

---
+++ app/Rules/Calls/VacantSessionTimeRule.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Rules\Calls;
+
+use App\Models\Call;
+use App\Models\CallSession;
+use Carbon\Carbon;
+use Illuminate\Contracts\Validation\Rule;
+
+class VacantSessionTimeRule implements Rule
+{
+    /**
+     * @var Call
+     */
+    protected $call;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @param Call $call
+     */
+    public function __construct(Call $call)
+    {
+        $this->call = $call;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $start = Carbon::parse(array_get($value, 'start'));
+        $end = Carbon::parse(array_get($value, 'end'));
+
+        return CallSession::query()
+                ->where('call_id', $this->call->getKey())
+                ->where(function ($query) use ($start, $end) {
+                    $query->where('start_at', '<', $start)
+                        ->where('end_at', '>', $end)
+                        ->orWhere('start_at', '>', $start)
+                        ->where('start_at', '<', $end)
+                        ->orWhere('end_at', '<', $end)
+                        ->where('end_at', '>', $start)
+                        ->orWhere('start_at', '>', $start)
+                        ->where('end_at', '<', $end);
+                })
+                ->doesntExist();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
